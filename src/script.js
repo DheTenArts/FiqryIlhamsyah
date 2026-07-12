3fcfbe7181ea133b5949021f864a96af09900736
DheTenArts <dhetenart01@gmail.com>
2026-07-12 23:54:19 +0800

    Memperbaiki Typing Js

--- src/script.js
+++ src/script.js
@@ -1,31 +1,25 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-var typed = new Typed("#typing", {
-    strings: [
-        "👋 Hi Semuanya!",
-        "👋 Selamat Datang di Portfolio Ku",
-        "👋 Senang Bertemu dengan Kalian!"
-    ],
-    typeSpeed: 70,
-    backSpeed: 40,
-    backDelay: 1500,
-    loop: true
+new Typed("#typing", {
+    strings: ["👋 Hi, Welcome to My Portfolio"],
+    typeSpeed: 60,
+    showCursor: false,
+    loop: false
 });
+
 
 new Typed("#typing-skill", {
     strings: [
-        "Mahasiswa Software Engineering Technology",
+        "Software Engineering Student",
         "Web Developer",
-        "3D Designer",
         "Graphic Designer",
-        "Logo dan Brand Designer",
-        "Pixelart Artist",
-        "Creative Problem Solver"
+        "PixelArt Artist",
+        "3D Designer"
     ],
     typeSpeed: 60,
     backSpeed: 35,
-    backDelay: 1800,
+    backDelay: 2000,
     loop: true
 });
 
