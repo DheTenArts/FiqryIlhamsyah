a18c4dd613b73509ef59fa55bca3c8eb4d25a670
DheTenArts <dhetenart01@gmail.com>
2026-07-12 23:21:33 +0800

    Memperibaiki Animate Slide, Dan Memperbaiki halaman keahlian dan pendidikan

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 var typed = new Typed("#typing", {
     strings: [
         "👋 Hi Semuanya!",
@@ -26,6 +29,10 @@ new Typed("#typing-skill", {
     loop: true
 });
 
-AOS.init({
-    once: true
+// AOS.init({
+//     once: true
+// });
+sal({
+    threshold: 0.15,
+    once: false
 });
